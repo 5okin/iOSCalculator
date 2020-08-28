1063b73cf4c375675cf8b40150d46ac2dd42e7ff
Nikos <nkoutsolelos@me.com>
2020-08-28 23:20:12 +0300

    Initial Commit

---
+++ iOSCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSCalculator
+//
+//  Created by Nikos Koutsolelos on 28/8/20.
+//  Copyright © 2020 Nikos Koutsolelos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
